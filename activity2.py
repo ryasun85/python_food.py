d999bb70db884edf174ead41e454e26e66a38565
ryasun85 <59563060+ryasun85@users.noreply.github.com>
2020-01-21 15:22:03 +0000

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,16 @@
+breakfast = "eggs"
+lunch = "salad"
+dinner = "sandwich"
+
+print("For breakfast I ate {}.".format(breakfast))
+print("For breakfast I ate {}.".format(lunch))
+print("For breakfast I ate {}.".format(dinner))
+
+breakfast = "porridge"
+lunch = "soup"
+dinner = "fruits"
+
+print("Tomorrow for breakfast I ate {}.".format(breakfast))
+print("Tomorrow for breakfast I ate {}.".format(lunch))
+print("Tomorrow for breakfast I ate {}.".format(dinner))
+
